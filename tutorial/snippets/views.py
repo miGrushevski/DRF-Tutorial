b5d3447f55b274646f3b160fdd21dd042708ff0a
Andrei Barushka <andrei.barushka@gmail.com>
2022-09-12 00:27:00 +0300

    Update "views.py" file (delete unused imports).

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,10 +1,6 @@
-from django.http import Http404
 from rest_framework import mixins
 from rest_framework import generics
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework.request import Request
-from rest_framework import status
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
